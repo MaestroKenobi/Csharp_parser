ce4cff0e371933292be807b53c06b9ee22e43881
Maestro Kenobi <mretrololoshka@gmail.com>
2019-05-07 16:54:57 +0300

    Ver 0.0.1.0

--- ConsoleVer/Program.cs
+++ ConsoleVer/Program.cs
@@ -41,41 +41,71 @@ namespace ConsoleVer
                     HtmlWeb web = new HtmlWeb();
                     var html_doc = web.Load(html);
                     HtmlNode name = html_doc.DocumentNode.SelectSingleNode("//body//h1");
-                    HtmlNode serial = html_doc.DocumentNode.SelectSingleNode("//text()[contains(., 'Серия 1-')]");
-                    string path_result = "result.txt";
-                    FileInfo file = new FileInfo(path_result);
-                    if (file.Exists)
+                    HtmlNode ep = html_doc.DocumentNode.SelectSingleNode("//text()[contains(., 'Серия 1-')]");
+                    string ep_str = ep.InnerText.ToString();
+                    int aa = ep_str.IndexOf('-');
+                    int bb = ep_str.IndexOf('[');
+                    string temp = ep_str.Substring(aa - 1);
+                    int cc = temp.IndexOf(' ');
+                    temp = temp.Remove(cc);
+                    string serial_last = temp.Substring(2);
+                    int ep_last = Convert.ToUInt16(serial_last);
+                    HtmlNode episodes_total = html_doc.DocumentNode.SelectSingleNode("//text()[contains(., 'ТВ')]");
+                    if (episodes_total == null)
                     {
-                        using (StreamReader reader = new StreamReader(path_result, Encoding.UTF8))
+                        episodes_total = html_doc.DocumentNode.SelectSingleNode("//text()[contains(., 'фильм')]");
+                        if (episodes_total == null)
                         {
-                            string line_result;
-                            while ((line_result = reader.ReadLine()) != null)
-                            {
-                                string result_past = line_result;
-                                if (result_past == Convert.ToString(serial))
-                                {
-
-                                }
-                                else
-                                {
-                                    string name_str = name.InnerHtml.Trim();
-                                    int a = name_str.IndexOf("<");
-                                    int b = name_str.LastIndexOf(">");
-                                    name_str = name_str.Insert(a, "/");
-                                    name_str = name_str.Remove(a + 1, 4);
-                                    Console.WriteLine(name_str);
-                                    Console.ReadKey();
-                                    //Just a comment
-                                }
-                            }
+                            episodes_total = html_doc.DocumentNode.SelectSingleNode("//text()[contains(., 'OVA')]");
                         }
                     }
-                    else
+                    string episodes_total_str = episodes_total.InnerText;
+                    int aaa = episodes_total_str.IndexOf("(");
+                    int bbb = episodes_total_str.IndexOf(")");
+                    episodes_total_str = episodes_total_str.Substring(aaa+1, bbb - aaa - 1);
+                    int ccc = episodes_total_str.IndexOf(" ");
+                    episodes_total_str = episodes_total_str.Remove(ccc);
+                    int eps_total = Convert.ToUInt16(episodes_total_str);
+                    if (ep_last != eps_total)
                     {
-                        file.Create();
-                        i = 0;
+
                     }
+                    Console.WriteLine(eps_total);
+                    Console.ReadKey();
                 }
+                //    string path_result = "result.txt";
+                //    FileInfo file = new FileInfo(path_result);
+                //    if (file.Exists)
+                //    {
+                //        using (StreamReader reader = new StreamReader(path_result, Encoding.UTF8))
+                //        {
+                //            string line_result;
+                //            while ((line_result = reader.ReadLine()) != null)
+                //            {
+                //                string result_past = line_result;
+                //                if (result_past == Convert.ToString(serial))
+                //                {
+
+                //                }
+                //                else
+                //                {
+                //                    string name_str = name.InnerHtml.Trim();
+                //                    int a = name_str.IndexOf("<");
+                //                    int b = name_str.LastIndexOf(">");
+                //                    name_str = name_str.Insert(a, "/");
+                //                    name_str = name_str.Remove(a + 1, 4);
+                //                    Console.WriteLine(name_str);
+                //                    Console.ReadKey();
+                //                }
+                //            }
+                //        }
+                //    }
+                //    else
+                //    {
+                //        file.Create();
+                //        i = 0;
+                //    }
+                //}
                 else if (html.Contains("sovetromantica.com"))
                 {
 
